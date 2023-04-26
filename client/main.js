19bed6fa0b37ced6dc0463eabe944feefb58c06b
yaswant2403 <yse2@illiois.edu>
2023-04-25 21:55:36 -0500

    Added Input Validation and OpenAI's Moderation

--- client/main.js
+++ client/main.js
@@ -19,80 +19,112 @@ const handleSubmit = async (e) => {
   let customFormat = format;
   if (format == 'custom' && customFormat) {
     customFormat = document.querySelector('#custom-format-input').value;
-    console.log(customFormat)
+    //console.log(customFormat)
   }
-  const sender = document.querySelector('#from').value;
-  const receiver = document.querySelector('#to').value;
-  const subject = document.querySelector('#subject').value;
+  const sender = document.querySelector('#from').value.trim();
+  const receiver = document.querySelector('#to').value.trim();
+  const subject = document.querySelector('#subject').value.trim();
   const length = document.querySelector('#length').value + " sentences";
   const include = document.querySelector('#include').value.trim();
-  const message_prompt = "Generate a " + customFormat + " to " + receiver + " from " 
-  + sender + " with the subject of \"" + subject + "\" that is " + length +".";
-  const image_prompt = "Write a DALL-E prompt to generate an image about " + subject // can have additional details once include input is made
-  + " and include " + include + ". " + "Your only response should be the DALL-E prompt without any quotations such that if I copy the response, I should be able" + 
-  " to paste it directly into the DALL-E prompt box."; 
-  console.log("How the prompt is in frontend: " + message_prompt);
-  console.log("Image prompt: " + image_prompt);
-  
-  // creating new div element to display response and clearing responseDiv
-  const displayResponse = document.createElement('div');
-  responseDiv.innerHTML = '';
-  // getting GIF Image and displaying it 
-  const loadingGIF = document.querySelector('#loadingGIF');
-  loadingGIF.style.display = 'block';
-
-  // Sending user prompt to backend
-  // local: http://localhost:5000
-  // cPanel: https://openapi.atlasaiteam.web.illinois.edu/
-  const chatResponse = await fetch('https://openapi.atlasaiteam.web.illinois.edu/',{
-    method: 'POST', // from server.js
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      message_prompt: message_prompt,
-      image_prompt: image_prompt
-    }) 
-  })
-
-  // Grabbing response from backend
-  if (chatResponse.ok) {
-    loadingGIF.style.display = "none";
-    const data = await chatResponse.json();
-    const finalResponse = data.bot.trim();
-    const finalImageUrl = data.image_bot;
-    console.log("How the response is in frontend: " + finalResponse);
-    // Creates a new element to display the results
-    displayResponse.innerHTML = `
-      <p>From: ${sender}</p>
-      <p>To: ${receiver}</p>
-      <p>Subject: ${subject}</p>
-      <p>Image Keywords: ${include}</p>
-      <p>Your Prompt: ${message_prompt}</p>
-      <h1>ChatGPT Response</h1>
-      <p>${finalResponse}</p>
-      <img src="${finalImageUrl}" class="mx-auto">
-      `
-    responseDiv.innerHTML = ''; //clear responseDiv before appending new response
-    responseDiv.appendChild(displayResponse);
+  // Input Validation of Null/Empty Strings
+  if (sender == null || sender == "" || receiver == null || receiver == "" || subject == null || subject == "" || length.includes('-') ||
+    include == null || include == "" || customFormat == null || customFormat == "") {
+      alert("Please Enter In All Required Fields");
+      form.reset();
   } else {
-    const err = await chatResponse.text();
-    displayResponse.innerHTML = `
-      <p>Something went wrong!</p>
-      <p>${err}</p>
-      `
-    loadingGIF.style.display = "none";
-    responseDiv.innerHTML = ''; //clear responseDiv before appending new response
-    responseDiv.appendChild(displayResponse);
+    const message_prompt = "Generate a " + customFormat + " to " + receiver + " from " 
+    + sender + " with the subject of \"" + subject + "\" that is " + length +".";
+    const image_prompt = "Write a DALL-E prompt to generate an image about " + subject // can have additional details once include input is made
+    + " and include " + include + ". " + "Your only response should be the DALL-E prompt without any quotations such that if I copy the response, I should be able" + 
+    " to paste it directly into the DALL-E prompt box."; 
+    console.log("How the prompt is in frontend: " + message_prompt);
+    console.log("Image prompt: " + image_prompt);
+    
+    // creating new div element to display response and clearing responseDiv
+    const displayResponse = document.createElement('div');
+    responseDiv.innerHTML = '';
+    // getting GIF Image and displaying it 
+    const loadingGIF = document.querySelector('#loadingGIF');
+    loadingGIF.style.display = 'block';
+
+    // Sending user prompt to backend
+    // local: http://localhost:5000
+    // cPanel: https://openapi.atlasaiteam.web.illinois.edu/
+    const chatResponse = await fetch('https://openapi.atlasaiteam.web.illinois.edu/',{
+      method: 'POST', // from server.js
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        message_prompt: message_prompt,
+        image_prompt: image_prompt
+      }) 
+    })
+
+    // Grabbing response from backend
+    if (chatResponse.ok) {
+      loadingGIF.style.display = "none";
+      const data = await chatResponse.json();
+      const finalResponse = data.bot.trim();
+      const finalImageUrl = data.image_bot;
+      console.log("How the response is in frontend: " + finalResponse);
+      if (finalResponse.includes("violates OpenAI's usage policies") || finalImageUrl.includes("violates OpenAI's usage policies")) {
+        displayResponse.innerHTML = `
+        <p>From: ${sender}</p>
+        <p>To: ${receiver}</p>
+        <p>Subject: ${subject}</p>
+        <p>Image Keywords: ${include}</p>
+        <p>Your Prompt: ${message_prompt}</p>
+        <h1>Message:</h1>
+        <p>${finalResponse}</p>
+        <h1>Image:</h1>
+        <p>${finalImageUrl}</p>
+        `
+      } else {
+        //Creates a new element to display the results
+        displayResponse.innerHTML = `
+          <p>From: ${sender}</p>
+          <p>To: ${receiver}</p>
+          <p>Subject: ${subject}</p>
+          <p>Image Keywords: ${include}</p>
+          <p>Your Prompt: ${message_prompt}</p>
+          <h1>ChatGPT Response</h1>
+          <p>${finalResponse}</p>
+          <img src="${finalImageUrl}" class="mx-auto">
+          `
+      }
+      responseDiv.innerHTML = ''; //clear responseDiv before appending new response
+      responseDiv.appendChild(displayResponse);
+    } else {
+      const err = await chatResponse.json();
+      const message = err.bot.trim();
+      // Means ChatGPT is down or API Key has run out of credits
+      if (message.includes("About Section!")) {
+        displayResponse.innerHTML = `
+        <p>Something went wrong!</p>
+        <p>${message}</p>
+        `
+      } else { // Image can't be generated, but the message can so we display that
+        const image_err = err.image_bot.trim();
+        displayResponse.innerHTML = `
+        <p>From: ${sender}</p>
+        <p>To: ${receiver}</p>
+        <p>Subject: ${subject}</p>
+        <p>Image Keywords: ${include}</p>
+        <p>Your Prompt: ${message_prompt}</p>
+        <h3>Message:</h3>
+        <p>${message}</p>
+        <h3>Image:</h3>
+        <p>${image_err}</p>
+        `
+      }
+      loadingGIF.style.display = "none";
+      responseDiv.innerHTML = ''; //clear responseDiv before appending new response
+      responseDiv.appendChild(displayResponse);
+    }
+    form.reset();
   }
-  form.reset();
 }
 
 // Once we submit on the form, we will call the handleSubmit Function
 form.addEventListener('submit', handleSubmit);
-// Just added bonus to submit the form by pressing enter key (key code 13)
-form.addEventListener('keyup', (e) => {
-  if (e.keyCode === 13) { // === ensures type matches as well
-    handleSubmit(e);
-  }
-});
