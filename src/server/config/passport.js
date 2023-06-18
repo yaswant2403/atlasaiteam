157aef0ab9429cd71e0915b8ff238d30415d94cb
yaswant2403 <yse2@illiois.edu>
2023-06-17 21:33:49 -0500

    Final comments for passport.js

--- src/server/config/passport.js
+++ src/server/config/passport.js
@@ -1,14 +1,17 @@
+/******************************************************************************
+ * Module dependencies and initialization.
+ *****************************************************************************/
 var passport = require("passport");
 var OIDCStrategy = require("passport-azure-ad").OIDCStrategy;
-var User = require('./db.config');
-
+var User = require('./db.config'); //grabbing User from database
+// checking database connection
 try {
     (async () => {
       await User.sync();
-      console.log('Passport use')
+      console.log('User synced to database in passport.js!')
     })();
   } catch (error) {
-    console.error('Unable to create table!', error);
+    console.error('Unable to sync database in passport.js!', error);
   }
 
 // for logging
@@ -17,18 +20,25 @@ var logger = bunyan.createLogger({
   name: 'Spotlight Generator Application'
 });
 
+// Setting up Passport
+//-----------------------------------------------------------------------------
+// To support persistent login sessions, Passport needs to be able to
+// serialize users into and deserialize users out of the session. In our case,
+// we are storing the user netID when serializing, and finding the user by netID 
+// when deserializing.
+//-----------------------------------------------------------------------------
 passport.serializeUser(function(user, done) {
     done(null, user.oid);
 });
   
 passport.deserializeUser(function(oid, done) {
-    User.findOne({
+    User.findOne({ // use Sequelize model's built-in method to find a single entry where oid = req.oid
         where: {
         oid: oid
         }
     }).then(function(user) {
         if(user) {
-        return done(null, user);
+            return done(null, user);
         }
     })
     .catch((err) => {
@@ -36,7 +46,10 @@ passport.deserializeUser(function(oid, done) {
         return done(err);
     });
 });
-  
+
+// Use the OIDCStrategy within Passport.
+
+// OIDC Configuration options (https://learn.microsoft.com/en-us/azure/active-directory/develop/v2-protocols-oidc)
 var OIDC_Configs = {
     identityMetadata: `https://login.microsoftonline.com/${process.env.TENANT_ID}/v2.0/.well-known/openid-configuration`,
     clientID: process.env.CLIENT_ID, // Required, the client ID of your app in AAD
@@ -50,7 +63,12 @@ var OIDC_Configs = {
     scope: ['openid', 'profile', 'email'],
     loggingLevel: 'info',
 };
-  
+
+// Strategies in passport require a `verify` function, which accepts credentials
+// (in this case, the `oid` claim in id_token), and invoke a callback to find
+// the corresponding user object. For this example, we simply add the user if they don't exist
+// in our database. However, for production, we will have a database already created
+// and if they don't exist in there, they won't be able to access our web application
 var verifyCallback = function (iss, sub, profile, accessToken, refreshToken, done) {
     if (!profile.oid) {
         console.log(profile);
@@ -65,17 +83,17 @@ var verifyCallback = function (iss, sub, profile, accessToken, refreshToken, don
         logger.info('we are using user: ', user);
         return done(null, user);
         } else {
-        var data = {
-            oid: profile.oid,
-            name: profile.displayName,
-            email: profile._json.email
-        }
-        User.create(data).then(function(newUser, created) {
-            console.log("Created a new user! Here they are:")
-            console.log(newUser.name);
-            console.log(newUser);
-            return done(null, newUser);
-        })
+            var data = {
+                oid: profile.oid,
+                name: profile.displayName,
+                email: profile._json.email
+            }
+            User.create(data).then(function(newUser, created) { // adding a new User to our database
+                console.log("Created a new user! Here they are:")
+                console.log(newUser.name);
+                console.log(newUser);
+                return done(null, newUser);
+            })
         }
     })
     .catch((err) => {
@@ -84,8 +102,9 @@ var verifyCallback = function (iss, sub, profile, accessToken, refreshToken, don
     });
 };
   
-// Using OIDC Strategy with custom configs and verify callback function
+// Making passport use our OIDC Strategy with custom configs and verify callback function
 const strategy = new OIDCStrategy(OIDC_Configs, verifyCallback);
 passport.use(strategy);
 
+//exporting passport object
 module.exports = passport;
