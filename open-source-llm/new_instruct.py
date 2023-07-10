f261644d13683fb5c30c487f777a72297f48457c
nickgong1 <nickgong123@outlook.com>
2023-07-09 23:40:36 -0500

    adding fine tune

--- open-source-llm/new_instruct.py
+++ open-source-llm/new_instruct.py
@@ -14,6 +14,14 @@ import utils
 
 import fire
 
+# @misc{alpaca,
+#   author = {Rohan Taori and Ishaan Gulrajani and Tianyi Zhang and Yann Dubois and Xuechen Li and Carlos Guestrin and Percy Liang and Tatsunori B. Hashimoto },
+#   title = {Stanford Alpaca: An Instruction-following LLaMA model},
+#   year = {2023},
+#   publisher = {GitHub},
+#   journal = {GitHub repository},
+#   howpublished = {\url{https://github.com/tatsu-lab/stanford_alpaca}},
+# }
 
 def encode_prompt(prompt_instructions):
     """Encode multiple prompt instructions into a single string."""
